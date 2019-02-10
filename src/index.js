2f4733995b84c221c33c6eafd243605a21c830a7
Ben Brunton <benbrunton@hotmail.com>
2019-02-10 21:59:04 +0000

    include react to get vertical slice of card game working

--- src/index.js
+++ src/index.js
@@ -1,45 +1,5 @@
-import('wasm-pusoy-dos').then((wasm) => {
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(<App />, document.getElementById("container"));
 
-    let game, player;
-    let hand = [];
-    try {
-        game = wasm.create_game(["a", "b"]);
-        console.log("created game");
-        console.log(game);
-    } catch(err) {
-        console.log("error creating game!");
-        console.error(err.message);
-    }
-
-    try {
-        player = wasm.get_player(game, "a");
-        console.log(player);
-    } catch (err) {
-        console.log("error grabbing player");
-        console.error(err);
-    }
-
-
-    let cardContainer = document.getElementById("card-holder");
-    let cards = player.map((card) => {
-        let c = JSON.stringify(card);
-        return `<div data-card='${c}'>
-                    ${card.card.rank} - ${card.card.suit.name}
-                </div>`;
-    }).join('');
-
-    cardContainer.innerHTML = cards;
-    cardContainer.addEventListener('click', (ev) => {
-        let card = JSON.parse(ev.target.dataset.card);
-        card.joker = false;
-        hand.push(card);
-    });
-
-    let button = document.getElementById("submit-button");
-    button.addEventListener('click', ()=>{
-        console.log(hand);
-        let selectedHand = wasm.get_hand_type(hand);
-        console.log(selectedHand);
-    });
-
-});
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+      }
+    ]
+  },
   devServer: {
     contentBase: './dist'
   },
