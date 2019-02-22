7779a080797fbde91c672b692006ccbd2055ed7e
Ben Brunton <benbrunton@hotmail.com>
2019-02-22 21:20:23 +0000

    enable ga acc to pull through env vars

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = env => merge({
+module.exports = merge({
   mode: 'development',
   devtool: 'inline-source-map',
   plugins: [
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = merge({
+module.exports = env => merge({
   mode: 'production',
   devtool: 'source-map',
   plugins: [
