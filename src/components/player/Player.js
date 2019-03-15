f410d411783f028a608974653c28fd676c318c9c
Roy Brunton <notnurbyor@googlemail.com>
2019-03-15 23:50:11 +0900

    rudimentary pinch to spread

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -17,21 +17,27 @@ const Player = ({ cards, onSelect }) => {
   const [ draggingLeft, setDraggingLeft ] = useState(0);
 
   const [ cardWidth, setCardWidth ] = useState(0);
-  const [ cardOverlap, setCardOverlap ] = useState(30);
 
-  const [ scrolling, setScrolling ] = useState(0);
-  const scroll = useRef(0);
+  const playerWidth = useRef(0);
+
+  // Need the ref to be able to get and set current value in effect
+  const scrollRef = useRef(0);
+  // Need state to make react rerender (changing ref doesn't trigger rerender)
+  const [ scroll, setScroll] = useState(0);
+
+  const cardOverlapRef = useRef(30);
+  const [ cardOverlap, setCardOverlap ] = useState(30);
 
   const playerRef = useRef(null);
 
   const dragObservables = useDragObservable(playerRef.current);
 
-  const width = cards.length * cardOverlap + (cardWidth - cardOverlap);
-
    useEffect(() => {
      const width = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--card-width'), 10);
      setCardWidth(width);
-     // setCardOverlap((document.querySelector('#container').offsetWidth - width) / cards.length);
+     cardOverlapRef.current = (document.querySelector('#container').offsetWidth - width) / cards.length;
+     setCardOverlap(cardOverlapRef.current);
+     playerWidth.current = cards.length * cardOverlapRef.current + (width - cardOverlapRef.current);
   }, []);
 
   useEffect(() => {
@@ -56,8 +62,7 @@ const Player = ({ cards, onSelect }) => {
     let _startX = 0;
     let _lastX = 0;
 
-    let _scroll = 0;
-    let _scrolling = 0;
+    let _lastTouches = [];
 
     const getOffset = e => {
       return e.targetTouches && e.targetTouches.length
@@ -65,9 +70,46 @@ const Player = ({ cards, onSelect }) => {
         : e.offsetX;
     }
 
+    const updateScroll = scrolled  => {
+      scrollRef.current = clampScroll(scrollRef.current + scrolled);
+      setScroll(scrollRef.current);
+    }
+
+    const getGesture = (touches, lastTouches) => {
+      if (touches.length > 1 && lastTouches.length > 1) {
+        let point1 = -1;
+        let point2 = -1;
+
+        for (let i = 0; i < lastTouches.length; i++) {
+          if (lastTouches[i].identifier === touches[0].identifier) { point1 = i; }
+          if (lastTouches[i].identifier === touches[1].identifier) { point2 = i; }
+        }
+
+        if (point1 >= 0 && point2 >= 0) {
+          const touchOneChange = touches[0].pageX - lastTouches[point1].pageX;
+          const touchTwoChange = touches[1].pageX - lastTouches[point2].pageX;
+
+          // check if swipe
+          if (touchOneChange * touchTwoChange > 0) {
+            return { type: "swipe", value: touches[0].pageX - lastTouches[point1].pageX };
+          }
+
+          const diff1 = Math.abs(touches[1].pageX - touches[0].pageX);
+          const diff2 = Math.abs(lastTouches[point2].pageX - lastTouches[point1].pageX);
+
+          if (Math.abs(diff1 - diff2) > 1 && diff1 !== diff2) {
+            return { type: "pinch", value: (diff1 - diff2) / 7 };
+          }
+
+          return { type: "none" };
+        }
+      }
+    }
+
     const startSubscription = start$.subscribe(start => {
       if (start.touches && start.touches.length > 1) {
-        _scroll = start.touches[0].pageX;
+        start.preventDefault();
+        _lastTouches = start.touches;
         return;
       }
 
@@ -79,33 +121,43 @@ const Player = ({ cards, onSelect }) => {
     });
     const dragSubscription = drag$.subscribe(move => {
       if (move.touches && move.touches.length > 1) {
-        _scrolling = move.touches[0].pageX - _scroll;
-        setScrolling(_scrolling);
+        move.preventDefault();
+        const gesture = getGesture(move.touches, _lastTouches);
+        switch (gesture.type) {
+          case "swipe":
+            updateScroll(gesture.value);
+            break;
+          case "pinch":
+            cardOverlapRef.current = Math.max((document.querySelector('#container').offsetWidth - cardWidth) / cards.length, Math.min(cardWidth, cardOverlapRef.current + gesture.value));
+            playerWidth.current = cards.length * cardOverlapRef.current + (cardWidth - cardOverlapRef.current);
+            setCardOverlap(cardOverlapRef.current);
+            updateScroll(0);
+          default:
+            break;
+        }
+        _lastTouches = [...move.touches];
         return;
       }
       const mouseX = getMouseX(move);
       const cardLeft = Math.min(
-        width - cardWidth,
+        playerWidth.current - cardWidth,
         Math.max(
           0,
           mouseX - _startX
         )
       );
-      const _moveIndex = getCardIndex(cardLeft + (cardOverlap / 2));
+      const _moveIndex = getCardIndex(cardLeft + (cardOverlapRef.current / 2));
       setDragging(true);
       setDraggingLeft(cardLeft);
       setMoveIndex(_moveIndex);
 
       // Scroll when dragging card to edges
-      const currentScroll = clampScroll(scroll.current + _scrolling);
-      const rightThreshold = playerRef.current.offsetWidth - currentScroll - (cardWidth * 1.25);
-      const leftThreshold = -currentScroll + (cardWidth / 2);
+      const rightThreshold = playerRef.current.offsetWidth - scrollRef.current - (cardWidth * 1.25);
+      const leftThreshold = -scrollRef.current + (cardWidth / 2);
       if (_lastX && cardLeft - _lastX > 0 && cardLeft > rightThreshold) {
-        _scrolling = Math.floor(rightThreshold - cardLeft);
-        scroll.current = clampScroll(scroll.current + _scrolling);
+        updateScroll(Math.floor(rightThreshold - cardLeft));
       } else if (_lastX && cardLeft - _lastX < 0 && cardLeft < leftThreshold) {
-        _scrolling = Math.floor(leftThreshold - cardLeft);
-        scroll.current = clampScroll(scroll.current + _scrolling);
+        updateScroll(Math.floor(leftThreshold - cardLeft));
       }
       _lastX = cardLeft;
 
@@ -114,9 +166,6 @@ const Player = ({ cards, onSelect }) => {
       }
     });
     const endSubscription = end$.subscribe(end => {
-      scroll.current = clampScroll(scroll.current + _scrolling);
-      _scrolling = 0;
-      setScrolling(_scrolling);
       setDragging(false);
     });
 
@@ -145,12 +194,12 @@ const Player = ({ cards, onSelect }) => {
   }
 
   function getCardIndex(x) {
-    return Math.max(0, Math.min(Math.floor(x / cardOverlap), cards.length - 1));
+    return Math.max(0, Math.min(Math.floor(x / cardOverlapRef.current), cards.length - 1));
   }
 
   function clampScroll(scroll) {
     const minScroll = playerRef.current
-      ? playerRef.current.parentElement.offsetWidth - (cards.length * cardOverlap + (cardWidth - cardOverlap))
+      ? playerRef.current.parentElement.offsetWidth - (cards.length * cardOverlapRef.current + (cardWidth - cardOverlapRef.current))
       : -Infinity;
     return Math.min(0, Math.max(minScroll, scroll));
   }
@@ -175,15 +224,13 @@ const Player = ({ cards, onSelect }) => {
     }
   }
 
-  const scrollLeft = clampScroll(scroll.current + scrolling);
-
   return (
     <div
       ref={playerRef}
-      className={css.player}
+      className={`${css.player} ${dragging ? css.reordering : ''}`}
       style={{
-        width: (cards.length * cardOverlap + (cardWidth - cardOverlap)) + 'px',
-        transform: `translate(${scrollLeft}px)`,
+        width: (cards.length * cardOverlapRef.current + (cardWidth - cardOverlapRef.current)) + 'px',
+        transform: `translate(${scroll}px)`,
       }}
     >
       {
@@ -191,7 +238,7 @@ const Player = ({ cards, onSelect }) => {
 
           const position = order.findIndex(key => key === card.id);
           const cardDragging = dragging && position === startIndex;
-          const left = cardDragging ? draggingLeft : position * cardOverlap;
+          const left = cardDragging ? draggingLeft : position * cardOverlapRef.current;
 
           return (
             <Card
@@ -199,6 +246,7 @@ const Player = ({ cards, onSelect }) => {
               card={card}
               selected={selected[card.id] !== undefined}
               dragging={cardDragging}
+              reordering={dragging}
               style={{zIndex: position, left: left + 'px'}}
               onClick={() => !dragged && selectCard(card, card.id)}
             />
