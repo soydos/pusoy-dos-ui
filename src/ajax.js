e620362cde781810209ad06e41fdc26187b6fc82
Ben Brunton <benbrunton@hotmail.com>
2019-06-10 00:01:21 +0100

    config passthrough working correctly

--- src/ajax.js
+++ src/ajax.js
@@ -3,17 +3,17 @@ export default (axios, uriRoot) => {
   const headers = {
   };
 
-  async function get(url, config) {
+  async function get(url, config = {}) {
     addHeaders(config);
     return await axios.get(`${uriRoot}${url}`, config)
   }
 
-  async function post(url, data, config) {
+  async function post(url, data, config = {}) {
     addHeaders(config);
     return await axios.post(`${uriRoot}${url}`, data, config)
   }
 
-  function addHeaders(config = {}){
+  function addHeaders(config){
     config.headers = config.headers || {};
     config.headers['Access-Control-Allow-Origin'] =  uriRoot;
     config.headers['Content-Type'] = 'application/json';
