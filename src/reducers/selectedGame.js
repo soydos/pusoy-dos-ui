6c3cdbbf6324534eedfe55c81a4ed61151a0a164
Ben Brunton <benbrunton@hotmail.com>
2019-12-02 22:15:33 +0000

    fix selected game users

--- src/reducers/selectedGame.js
+++ src/reducers/selectedGame.js
@@ -4,10 +4,10 @@ export default function (state = {}, action) {
   switch (action.type) {
     case GAME_LOADED:
       return {
+        ...state,
         created: action.data.created_user,
         inGame: action.data.in_game,
         users: action.data.game_details.users,
-        ...state
       };
     default:
       return state
